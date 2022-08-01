f6134f0e63258e8792ff99d7b5cabcd857206316
Alana <halo2382001@yahoo.com>
2022-08-01 19:01:02 -0400

    working on splitting up the assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@
 const path = require("path");
 // Because this plugin is built into webpack, be sure we're bringing webpack's methods and properties into the config file
 const webpack = require("webpack");
+// import webpack bundle-analyzer
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   // the entry point is the root of the bundle and beginning of the dependency graph
@@ -11,13 +14,17 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.bundle.js",
   },
-  plugins:[
+  plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
     }),
+    // will analyze our bundle sizes to see how much JavaScript is being processed by the browser
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the static requires a report to output to an report.HTML file in the dist folder, 
+      // can also set to disable to temporarily stop the reporting and automatic opening of the report
+    }),
   ],
-  
   // mode is the mode we run the webpack, usually "production"
   mode: "development",
 };
