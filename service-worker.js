cf4dc34b428b2a195bd3b9af87b606f434af00a7
Alana <halo2382001@yahoo.com>
2022-08-03 17:42:55 -0400

    finished offline-functionality

--- service-worker.js
+++ service-worker.js
@@ -32,3 +32,54 @@ self.addEventListener("install", function (e) {
     })
   );
 });
+
+// add event listener to the activate event
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    // keys returns an array of all cache names which is called keylist here
+    caches.keys().then(function (keyList) {
+      // must filter out the caches from all cache names in <username>.github.io
+      let cacheKeeplist = keyList.filter(function (key) {
+        // captures then caches for just the onesn with tis app prefix
+        return key.indexOf(APP_PREFIX);
+      });
+      // add the current cache to the keeplist - cache_name is a global variable
+      cacheKeeplist.push(CACHE_NAME);
+
+      // returns a Promise that resolves once all old versions of the cache have been deleted.
+      return Promise.all(
+        keyList.map(function (key, i) {
+          // will only return a value of -1 if this item is not found in the keep list
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            // if the item is not found in the keep list, delete it from the cache
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+// retrieve information from cache
+// Here, we listen for the fetch event, log the URL of the requested resource, and then begin to define how we will respond to the request.
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  // respondwith intercepts the fetch request
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      // .match() to determine if the resource already exists in caches
+      if (request) {
+        console.log("responding with cache : " + e.request.url);
+        return request;
+        // if the resource is not in caches, we allow the resource to be retrieved from the online network as usual
+      } else {
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+
+      // You can omit if/else above for console.log & put just one line below like this too.
+      //   return request || fetch(e.request)
+    })
+  );
+});
